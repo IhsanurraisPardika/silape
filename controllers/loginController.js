925296975ec305a23856a07057a1d2a1cff37a37
IhsanurraisPardika <ihsanurrais06@gmail.com>
2026-01-12 11:13:21 +0700

    login ke home

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -5,3 +5,29 @@ exports.getlogin = (req, res) => {
     appName: 'SILAPE'
   });
 };
+
+exports.postlogin = (req, res) => {
+  const { email, password } = req.body;
+  
+  // TODO: Validasi email dan password dengan database
+  // Untuk sekarang, arahkan langsung ke home
+  if (email && password) {
+    // Dalam implementasi real, validasi dengan database Prisma
+    res.redirect('/home');
+  } else {
+    res.render('login', {
+      title: 'Login - SILAPE',
+      error: 'Email dan password harus diisi'
+    });
+  }
+};
+
+exports.gethome = (req, res) => {
+  res.render('home', {
+    title: 'Home - SILAPE',
+    user: {
+      name: 'IHSANURRAIS PARDIKA',
+      team: 'TIM 1'
+    }
+  });
+};
--- routes/login.js
+++ routes/login.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const loginController = require('../controllers/loginController');
 
 router.get('/', loginController.getlogin);
+router.post('/login', loginController.postlogin);
+router.get('/home', loginController.gethome);
 
 module.exports = router;
